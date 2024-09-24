7d2172efbb85f58f97992616e3ff5da73f51051a
LauraLuppi <lauravtluppi15@gmail.com>
2024-09-24 11:29:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+function disableOptions(questionName) {
+    let options = document.getElementsByName(questionName);
+    options.forEach(option => {
+        if (!option.checked) {
+            option.disabled = true;
+        }
+    });
+}
+
+function playSound() {
+    let clickSound = document.getElementById('selecionaSom');
+    clickSound.play();
+}
+
+function submitQuiz() {
+    // Desabilita o botão de envio após ser clicado
+    let enviarBtn = document.getElementById('enviarBtn');
+    enviarBtn.disabled = true;
+
+    // Habilita o botão de "Responder novamente"
+    let resetBtn = document.getElementById('resetBtn');
+    resetBtn.disabled = false;
+
+    let correctAnswers = {
+        q1: "B",
+        q2: "C",
+        q3: "B",
+        q4: "B",
+        q5: "B",
+        q6: "B",
+        q7: "B",
+        q8: "B",
+        q9: "B",
+        q10: "C",
+    };
+
+    let form = document.getElementById('quiz-form');
+    let score = 0;
+
+    for (let key in correctAnswers) {
+        let userAnswers = form.elements[key].value;
+        if (userAnswers === correctAnswers[key]) {
+            score++;
+        }
+    }
+
+    let result = document.getElementById('result');
+    result.innerHTML = `Você acertou ${score} de 10 perguntas.`;
+
+   
+    if (score === 10) {
+        let successSound = document.getElementById('venceuSom');
+        successSound.play();
+    } else {
+        let errorSound = document.getElementById('perdeuSom');
+        errorSound.play();
+    }
+}
+
+function resetQuiz() {
+    // Reabilita o botão de envio quando o quiz é reiniciado
+    let enviarBtn = document.getElementById('enviarBtn');
+    enviarBtn.disabled = false;
+
+    // Desabilita o botão de "Responder novamente" novamente
+    let resetBtn = document.getElementById('resetBtn');
+    resetBtn.disabled = true;
+
+    // Limpa as respostas e reseta o formulário
+    document.getElementById('quiz-form').reset();
+
+    // Reabilita as opções de resposta
+    let options = document.querySelectorAll('input[type="radio"]');
+    options.forEach(option => option.disabled = false);
+
+    // Limpa o resultado
+    let result = document.getElementById('result');
+    result.innerHTML = "";
+}
+
+
+
+
